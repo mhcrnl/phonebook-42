0d3054beef86881ea11de99d373e4d0847a5c7c2
snehashinde1996 <shindesa14.comp@coep.ac.in>
2015-10-27 13:13:40 -0400

    Update phonebook.h

--- phonebook.h
+++ phonebook.h
@@ -1,8 +1,7 @@
-
 typedef struct phonebook {
 	char fname[35];
 	char mname[35];
-	char lname[30];
+	char lname[35];
 	double mobile_no;
 	char address[50];
 	char sex[8];
@@ -18,8 +17,11 @@ int deleterecord(phonebook *p);
 int searchrecord(phonebook *p);
 int save(char *data);
 int backup(phonebook *p);
+int adminsignup(char *password, char *temp);
+int adminsignin(char *password);
 void exitapp();
-void login();
+void returnfunc();
+void t_display();
 phonebook *isavailable(phonebook *p, double mobile_no);
 int groupassign(phonebook *p);
 
